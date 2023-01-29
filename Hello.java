b6ac95d5538698e833f299e02efd48a326d1d4dd
edgaryum <116030333+edgaryum@users.noreply.github.com>
2023-01-29 18:32:07 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        int a = 1;
+        double b = 2.5;
+        double c = a + b;
+        String arequipe = "30";
+        System.out.println(c + " " + arequipe);
+    }
+}
