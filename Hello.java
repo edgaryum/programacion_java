98c546372f0970290f701fa949ab200e20c0a791
edgaryum <116030333+edgaryum@users.noreply.github.com>
2023-02-01 21:04:39 -0500

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,9 +1,46 @@
+import java.text.ParseException;
+import java.util.*;
+import java.util.StringTokenizer;
+
 public class Hello {
     public static void main(String[] args) {
-        int a = 1;
-        double b = 2.5;
-        double c = a + b;
-        String arequipe = "30";
-        System.out.println(c + " " + arequipe);
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("ingrese el numero a multiplicar");
+        String num3 = scanner.nextLine();
+        System.out.println("tu numero es "+num3);*/
+        String num3 = "1 2 3 * +";
+        StringTokenizer tokenizer = new StringTokenizer(num3);
+        Stack<Double> stack = new Stack<Double>();
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            System.out.println(token);
+            try{        
+                double num = Double.parseDouble(token);
+                stack.push(num);
+            }catch (NumberFormatException e){
+                // Si es un operador, saca los últimos dos números de la pila, aplica el operador y empuja el resultado de vuelta a la pila
+                double num2 = stack.pop();
+                double num1 = stack.pop();
+                double result = 0;
+                    switch(token){
+                    case "*":
+                        result = num1 * num2;
+                    break;
+                    case "/":
+                        result = num1 / num2;
+                    break;
+                    case "+":
+                        result = num1 + num2;
+                    break;
+                    case "-":
+                        result = num1 - num2;
+                    break;
+                    }
+                stack.push(result);
+            }
+        }
+        double result = stack.pop();
+        System.out.println(result);
+
     }
 }
